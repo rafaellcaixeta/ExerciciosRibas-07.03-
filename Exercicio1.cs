8fccab37cb09a2c26e7ed575ada1c3cafd2c9069
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-07 08:32:11 -0300

    Add files via upload

---
+++ Exercicio1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Atividade1
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Quantos números você vai digitar? ");
+        int n = int.Parse(Console.ReadLine());
+        double[] numeros = new double[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Digite o valor {i + 1}: ");
+            numeros[i] = double.Parse(Console.ReadLine());
+        }
+        double maiorValor = numeros[0];
+        int posicao = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (numeros[i] > maiorValor)
+            {
+                maiorValor = numeros[i];
+                posicao = i;
+            }
+        }
+
+        Console.WriteLine($"\nMAIOR VALOR = {maiorValor}");
+        Console.WriteLine($"POSIÇÃO DO MAIOR VALOR = {posicao}");
+    }
+}
