82057f5d0e18e3c0093caeff2c0f25fdbf1b09bf
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-07 17:36:52 -0300

    Update Exercicio4.cs

--- Exercicio4.cs
+++ Exercicio4.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite a quantidade de números reais no vetor: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[] numeros = new double[n];
+        double soma = 0;
+
+        Console.WriteLine($"Digite os {n} números reais:");
+        for (int i = 0; i < n; i++)
+        {
+            numeros[i] = double.Parse(Console.ReadLine());
+            soma += numeros[i];
+        }
+
+        double media = soma / n;
+
+        Console.WriteLine($"\nMédia aritmética: {media:F3}");
+        Console.WriteLine("Números abaixo da média:");
+
+        for (int i = 0; i < n; i++)
+        {
+            if (numeros[i] < media)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+    }
+}
