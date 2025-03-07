6b171f1784fdb85e4ad9cf61b5103fbf0338c436
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-07 09:10:16 -0300

    Add files via upload

---
+++ Exercicio5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite a quantidade de números inteiros no vetor: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[n];
+        int somaPares = 0;
+        int contPares = 0;
+
+        Console.WriteLine($"Digite os {n} números inteiros:");
+        for (int i = 0; i < n; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+
+            if (numeros[i] % 2 == 0)
+            {
+                somaPares += numeros[i];
+                contPares++;
+            }
+        }
+
+        if (contPares > 0)
+        {
+            double mediaPares = (double)somaPares / contPares;
+            Console.WriteLine($"\nMédia aritmética dos números pares: {mediaPares:F1}");
+        }
+        else
+        {
+            Console.WriteLine("\nNenhum número par foi digitado.");
+        }
+    }
+}
