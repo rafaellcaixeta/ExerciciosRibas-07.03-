152b422ac797f2b7349efd667d5af8e6077722eb
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-07 09:14:56 -0300

    Add files via upload

---
+++ Exercicio6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite a quantidade de pessoas: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] nomes = new string[n];
+        int[] idades = new int[n];
+
+        Console.WriteLine("Digite os nomes e idades das pessoas:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Nome da {i + 1}ª pessoa: ");
+            nomes[i] = Console.ReadLine();
+            
+            Console.WriteLine($"Idade de {nomes[i]}: ");
+            idades[i] = int.Parse(Console.ReadLine());
+        }
+
+        int indiceMaisVelho = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (idades[i] > idades[indiceMaisVelho])
+            {
+                indiceMaisVelho = i;
+            }
+        }
+
+        Console.WriteLine($"\nPessoa mais velha: {nomes[indiceMaisVelho]}");
+    }
+}
