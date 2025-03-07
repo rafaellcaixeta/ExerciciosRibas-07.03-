e3b0a1dea75fb3b4d5f9135c07c0dcf9f2c445ca
rafaellcaixeta <rafaellucianocaixeta@hotmail.com>
2025-03-07 08:45:31 -0300

    Add files via upload

---
+++ Exercicio2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Quantos números você deseja inserir?");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[n];
+        List<int> pares = new List<int>();
+
+        Console.WriteLine($"Digite os {n} números inteiros:");
+        for (int i = 0; i < n; i++)
+        {
+            numeros[i] = int.Parse(Console.ReadLine());
+            if (numeros[i] % 2 == 0)
+            {
+                pares.Add(numeros[i]);
+            }
+        }
+
+        Console.WriteLine("Números pares:");
+        foreach (int par in pares)
+        {
+            Console.Write(par + " ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Quantidade de números pares: {pares.Count}");
+    }
+}
